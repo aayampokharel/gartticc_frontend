0e95ccd3373fec636a13fd21f870815669f93cf1
Pokharel_Aayam <aayampokharel02@gmail.com>
2024-06-11 18:28:40 +0545

    updated the class AnimationService for animation method

---
+++ lib/canvas/AnimationService.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+class AnimationService {
+  Future<String> forProgressBar() async {
+    var response =
+        await http.get(Uri.parse("http://localhost:8080/progressbar"));
+    return response.body;
+  }
+}
